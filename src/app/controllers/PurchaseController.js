3cce099825d4a51abed16abe7facbe5099980416
Thiago Novato <thiagonovato@gmail.com>
2019-04-21 20:25:12 -0300

    Enviando emails com template.

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"Thiago Novato" <thiago@thiagonovato.com.br',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
